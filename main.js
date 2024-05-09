ff72e101c0c6e02b3d3ddeb65788244a1e3b9768
Nikita Kubarko <n.kubarko@naveksoft.com>
2024-05-09 11:59:11 +0300

    Initial

---
+++ main.js
@@ -0,0 +1,27 @@
+window.addEventListener('popstate', e => {
+  console.log(e);
+  console.log(location, location.pathname);
+  console.log(history);
+})
+
+const links = document.querySelectorAll('#nav a')
+console.log(links);
+links.forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    const path = link.getAttribute('href')
+    const origin = location.origin
+
+    history.pushState({}, '', `${origin}${path}`)
+  })
+})
+
+const nativePushState = history.pushState.bind(history);
+history.pushState = function(data, unused, url) {
+  console.log('pushState', data, url);
+  
+  nativePushState({
+    ...data,
+    debug: true
+  }, unused, url)
+}
